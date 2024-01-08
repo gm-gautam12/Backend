3223e2a76f5cd2ff07fccd1a43529f6d0d1af711
Gautam Mishra <njoygautam12@gmail.com>
2024-01-08 10:21:43 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("MongoDB connection failed !!!",err);
+});
