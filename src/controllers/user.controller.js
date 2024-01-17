d6da721be8c9bd978ec69adf0d57e1ceca8082f3
Gautam Mishra <njoygautam12@gmail.com>
2024-01-17 10:48:39 +0530

    testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,7 @@ const registerUser = asyncHandler( async(req,res) => {
 
     //check user exist or not
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username },{ email }]
     });
 
@@ -38,7 +38,12 @@ const registerUser = asyncHandler( async(req,res) => {
 
    // console.log(req.body);
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+      coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     //image validation
     if(!avatarLocalPath)
@@ -69,7 +74,7 @@ const registerUser = asyncHandler( async(req,res) => {
   )
 
   if(!createdUser){
-    throw new ApiError(500,"something went wrong while registering");
+    throw new ApiError(500,"something went wrong while registering please try again!!");
   }
 
   //sending response
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,12 +10,14 @@ cloudinary.config({
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
+        if(!localFilePath)return null;
         const response = await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
         //file has been uploaded successfully
-        console.log("file upload successfull",response.url);
-        return response;
+        // console.log("file upload successfull",response.url);
+        fs.unlinkSync(localFilePath)
+         return response;
 
     } catch (error) {
         fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
