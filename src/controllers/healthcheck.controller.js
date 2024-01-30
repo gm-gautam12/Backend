50df84768e4b31de4e5217924cf4d1e9ad00df1a
Gautam Mishra <njoygautam12@gmail.com>
2024-01-31 01:15:29 +0530

    documented

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -4,6 +4,9 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const healthCheck = asyncHandler(async(req,res) => {
      //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+     res.status(200).json(
+          new ApiResponse(200,"OK","Successfull")
+     );
+});
 
 export {healthCheck};
